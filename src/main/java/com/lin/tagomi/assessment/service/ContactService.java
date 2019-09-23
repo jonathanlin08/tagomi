08fbf3ecf11fd36d6e7f8d19e12b6bb09c04b33e
jlin <jonathanlin08@gmail.com>
2019-09-23 00:52:12 -0400

    remove txt file and dynamically download via resttemplate

--- src/main/java/com/lin/tagomi/assessment/service/ContactService.java
+++ src/main/java/com/lin/tagomi/assessment/service/ContactService.java
@@ -2,14 +2,10 @@ package com.lin.tagomi.assessment.service;
 
 import com.lin.tagomi.assessment.data.structure.GenericTrie;
 import com.lin.tagomi.assessment.entity.Contact;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -18,20 +14,28 @@ import java.util.stream.Collectors;
 @Component
 public class ContactService {
 
-    @Value("classpath:addresses.properties")
-    private Resource res;
-
     private List<Contact> contactList;
-
     private GenericTrie<Character> firstNameTrie;
     private GenericTrie<Character> lastNameTrie;
     private GenericTrie<Integer> phoneTrie;
 
     @PostConstruct
-    public void init() throws IOException {
+    public void init() {
         this.contactList = new ArrayList<>();
-        Files.lines(Paths.get(res.getURI())).forEach(line -> {
-            String[] tokens = line.split("\\t");
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl
+                = "http://coderpad-test-files.s3-website-us-east-1.amazonaws.com/addresses.txt";
+        String responseData
+                = restTemplate.getForEntity(fooResourceUrl, String.class).getBody();
+
+        this.initContactList(responseData);
+        this.initTries();
+    }
+
+    private void initContactList(String responseData) {
+        String[] rows = responseData.split("\\n");
+        for (String row : rows) {
+            String[] tokens = row.split("\\t");
             if (tokens.length == 3) {
                 String name = tokens[0];
                 String phone = tokens[1];
@@ -39,8 +43,7 @@ public class ContactService {
                 Contact c = new Contact(name, phone, address);
                 this.contactList.add(c);
             }
-        });
-        this.initTries();
+        }
     }
 
     public void initTries() {
