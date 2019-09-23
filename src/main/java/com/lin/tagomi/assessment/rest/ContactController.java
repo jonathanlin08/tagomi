f59262110d52515806f871adf624010a6f0bab2d
jlin <jonathanlin08@gmail.com>
2019-09-23 01:18:55 -0400

    controller enhancement

--- src/main/java/com/lin/tagomi/assessment/rest/ContactController.java
+++ src/main/java/com/lin/tagomi/assessment/rest/ContactController.java
@@ -9,20 +9,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ContactController {
     @Autowired
     private ContactService contactService;
 
-    @GetMapping("getTotalCount")
-    public ResponseEntity<Integer> getAllAppointments() {
+    @GetMapping("/count/total")
+    public ResponseEntity<Integer> getAllEntries() {
         return ResponseEntity.ok(contactService.getContactList().size());
     }
 
-    @PostMapping("/search/number/{term}")
-    public ResponseEntity<Iterable<Contact>> searchContactByPhoneTerm(@PathVariable Integer phoneTerm) {
-        contactService.searchPhone(phoneTerm);
-        return null;
+    @GetMapping("/count/name/{name}")
+    public ResponseEntity<Integer> countNameMatch(@PathVariable String name) {
+        return ResponseEntity.ok(this.searchContactByName(name).getBody().size());
+    }
+
+    @GetMapping("/count/phone/{areacode}")
+    public ResponseEntity<Integer> countNameMatch(@PathVariable Integer areacode) {
+        return ResponseEntity.ok(this.searchContactByPhoneTerm(areacode).getBody().size());
+    }
+
+    @PostMapping("/search/phone/{areacode}")
+    public ResponseEntity<List<Contact>> searchContactByPhoneTerm(@PathVariable Integer areacode) {
+        List<Contact> results = contactService.searchPhone(areacode);
+        return ResponseEntity.ok(results);
+    }
+
+    @PostMapping("/search/name/{term}")
+    public ResponseEntity<List<Contact>> searchContactByName(@PathVariable String term) {
+        List<Contact> results = new ArrayList<>();
+        results.addAll(contactService.searchFirstName(term));
+        results.addAll(contactService.searchLastName(term));
+        return ResponseEntity.ok(results);
     }
 
 }
